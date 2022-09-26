75dc8091979d39ab3b88a25fdea6daf20f3a774c
Irina <countess.all@yandex.ru>
2022-09-26 21:37:34 +0300

    вторая задача

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     2, 4 -> 16
 */
 
+/*
 Console.Write("Введите основание степени ");
 int x = int.Parse(Console.ReadLine());
 Console.Write("Введите показатель степени ");
@@ -15,5 +16,29 @@ int F(int A, int B)
     int s = 1;
     for(int i=1; i<=B; i++)
         s*=A;
+    return s;     
+}
+*/
+
+
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    452 -> 11
+    82 -> 10
+    9012 -> 12
+*/
+
+Console.Write("Введите число ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine($"сумма цифр числа {x} = {Sum(x)}");
+
+int Sum(int n)
+{
+    int s = 0;
+    while(n > 0)
+    {
+        s += n % 10;
+        n /= 10;
+    }
     return s;     
 }
